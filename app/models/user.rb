ebafc88c693ff1c664e208c5551e5ac4d1f4d193
Manu Phatak <manu@thinkcerca.com>
2016-11-19 18:07:26 -0600

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :posts
   has_many :comments
+
+  validates_presence_of :username, :email
+  validates_uniqueness_of :username, :email, case_sensitive: false
 end
